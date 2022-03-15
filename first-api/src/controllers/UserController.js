528f10d8dabaed5cb0776194559d7d0fce826511
William Koller <williamkoller404@gmail.com>
2022-03-14 23:19:14 -0300

    feat: add get user by id

--- first-api/src/controllers/UserController.js
+++ first-api/src/controllers/UserController.js
@@ -2,9 +2,25 @@ const users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response) {
-    response.writeHead(200, {
-      'Content-type': 'application/json',
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === 'desc') {
+        return a.id < b.id ? 1 : -1;
+      }
+      return a.id > b.id ? 1 : -1;
     });
-    response.end(JSON.stringify(users));
+
+    response.send(200, sortedUsers);
+  },
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      return response.send(404, { error: 'User not found.' });
+    }
+    response.send(200, user);
   },
 };
--- first-api/src/index.js
+++ first-api/src/index.js
@@ -1,27 +1,47 @@
 const http = require('http');
+const { URL } = require('url');
+
 const routes = require('./routes');
 
+const port = 3000;
 const server = http.createServer((request, response) => {
   const method = request.method;
-  const endpoint = request.url;
+  const parsedUrl = new URL(`http://localhost:${port}${request.url}`);
+  console.log(`Request method: ${method} | Endpoint : ${parsedUrl.pathname}`);
 
-  console.log(`Request method: ${method} | Endpoint : ${endpoint}`);
+  let { pathname } = parsedUrl;
+
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
 
   const route = routes.find((routeObj) => {
-    routeObj.endpoint === endpoint && routeObj.method === method;
+    return routeObj.endpoint === pathname && routeObj.method === method;
   });
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, {
       'Content-type': 'text/html',
     });
-    response.end(`Cannot ${method} ${endpoint}`);
+    response.end(`Cannot ${method} ${pathname}`);
   }
 });
-
-const port = 3000;
 
 server.listen(port, () =>
   console.log(`Server started at http://localhost:${port}`)
