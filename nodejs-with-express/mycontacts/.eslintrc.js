1ca57f7f24bc68b9aaca9973fce4bd1ca55785cf
William Koller <williamkoller404@gmail.com>
2022-03-18 08:56:19 -0300

    chore: add eslint

---
+++ nodejs-with-express/mycontacts/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+  },
+};
