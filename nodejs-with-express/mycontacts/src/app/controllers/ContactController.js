212f397084e40510b034a6a46c57a4fea8cc209c
William Koller <williamkoller404@gmail.com>
2022-03-20 22:08:27 -0300

    feat: add show and delete ContactController

--- nodejs-with-express/mycontacts/src/app/controllers/ContactController.js
+++ nodejs-with-express/mycontacts/src/app/controllers/ContactController.js
@@ -7,8 +7,15 @@ class ContactController {
     response.status(statusCode.OK).json(contacts);
   }
 
-  show() {
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
 
+    if (!contact) {
+      response.status(statusCode.NOT_FOUND).json({ error: 'Contact not found.' });
+    }
+
+    response.status(statusCode.OK).json(contact);
   }
 
   store() {
@@ -19,8 +26,19 @@ class ContactController {
 
   }
 
-  delete() {
+  async delete(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
 
+    if (!contact) {
+      response
+        .status(statusCode.NOT_FOUND)
+        .json({ error: 'Contact not found.' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(statusCode.NOT_CONTENT);
   }
 }
 
