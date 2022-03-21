1493030da26b15f6779833f4d29e5f2f6f18f72f
William Koller <williamkoller404@gmail.com>
2022-03-20 22:07:44 -0300

    feat: add router show and delete contact

--- nodejs-with-express/mycontacts/src/routes.js
+++ nodejs-with-express/mycontacts/src/routes.js
@@ -3,6 +3,8 @@ const contactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get('/', contactController.index);
+router.get('/contacts', contactController.index);
+router.get('/contacts/:id', contactController.show);
+router.delete('/contacts/:id', contactController.delete);
 
 module.exports = router;
